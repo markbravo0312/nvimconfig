882e4c86669a1aa83a53ebc21ce510bdda3a42a5
Mark <markbravo0312@gmail.com>
2023-12-13 21:54:54 +0100

    Changes

--- init.lua
+++ init.lua
@@ -1,47 +1,21 @@
-require('vars')         -- Variables
-require('opts')         -- Options
-require('keys')         -- Keymaps
-require('plugin')       --plugins
-vim.opt.autoindent = true          
--- window-scoped
-vim.opt.cursorline = true
-    
--- global scope
-vim.opt.autowrite = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-vim.opt.smarttab = true
-vim.opt.shiftround = true
-vim.opt.syntax = 'enable'
-vim.opt.display='lastline'
-vim.opt.linebreak = true
-vim.opt.wrap = true
+require "core"
 
---User Interface Options
-vim.opt.laststatus=2
-vim.opt.ruler = true
-vim.opt.wildmenu = true
-vim.opt.tabpagemax=50
-vim.opt.background='dark'
-vim.opt.cursorline = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.visualbell = true
-vim.opt.mouse='a'
-vim.opt.title = true
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
---Code Folding
-vim.opt.foldnestmax=3
+if custom_init_path then
+  dofile(custom_init_path)
+end
 
---Miscellaneous Options
-vim.opt.autoread=true
---vim.opt.backspace=indent,eol,start
-vim.opt.confirm=true
-vim.opt.hidden=true
-vim.opt.history=1000
-vim.opt.spell = true
+require("core.utils").load_mappings()
 
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
-vim.api.nvim_set_option("clipboard","unnamed")
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
